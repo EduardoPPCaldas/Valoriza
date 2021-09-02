9afd84c96991a9210576e444aa60e0bf3a9b5d41
Eduardo Caldas <eduardocaldas.dev@gmail.com>
2021-09-02 18:36:51 -0300

    Aula 03 - completed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
 import "reflect-metadata"
 import "./database"
+import "express-async-errors"
 import { router } from "./routes"
 
 const app = express()
@@ -8,6 +9,19 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, req: Request, res: Response ,next: NextFunction)=>{
+  if(err instanceof Error){
+    return res.status(400).json({
+      error: err.message
+    })
+  }
+
+  return res.status(500).json({
+    "status": "error",
+    "message": "Internal server error"
+  })
+})
 
 app.listen(3000, ()=>{
   console.log("Server is running")
